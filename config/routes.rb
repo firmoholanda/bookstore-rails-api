3fe413341df848a25174596d909a135b7d86c41c
firmo holanda <firmo@viize.com>
2020-08-31 16:20:54 -0300

    add Users, Books and Favourites controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/Books'
+  get 'users/Favourites'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
