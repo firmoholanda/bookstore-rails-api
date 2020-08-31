6d6b6ca8888d6faa3cb77b019f70a8430a2dfaf1
firmo holanda <firmo@viize.com>
2020-08-31 15:43:26 -0300

    initialize spec

---
+++ spec/rails_helper.rb
@@ -0,0 +1,32 @@
+# require database cleaner at the top level
+require 'database_cleaner'
+
+# [...]
+# configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+# [...]
+RSpec.configure do |config|
+  # [...]
+  # add `FactoryBot` methods
+  config.include FactoryBot::Syntax::Methods
+
+  # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  # start the transaction strategy as examples are run
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+  # [...]
+end
