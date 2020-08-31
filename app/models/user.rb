adad6b280894f537f1cce549fc499fd58a36ce38
firmo holanda <firmo@viize.com>
2020-08-31 16:15:48 -0300

    add belongs_to, associations and validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  has_many :books, dependent: :delete_all
+  has_many :favourites
+  has_many :favourite_books, through: :favourites, source: :book
+
+  validates_presence_of :name, :email, :password
+  validates :name, length: { minimum: 3 }
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 5 }
 end
