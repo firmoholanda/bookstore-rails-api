f9dc8e175b120d046eb6cd5352610d2a162b76b7
firmo holanda <firmo@viize.com>
2020-08-31 16:15:26 -0300

    add belongs_to, associations and validation to book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,10 @@
 class Book < ApplicationRecord
+  belongs_to :user
+
+  has_many :favourites, dependent: :delete_all
+  has_many :favourited_by, through: :favourites, source: :user
+
+  validates_presence_of :title, :description, :category
+
+  validates :title, length: { in: 5..30 }
 end
