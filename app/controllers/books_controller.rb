3fbccbdb06bc1e966243b2a19cf4abde9e1b7d44
firmo holanda <firmo@viize.com>
2020-08-31 16:41:54 -0300

    create controllers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,25 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show]
+
+  def index
+    @books = Book.all
+    json_response(@books)
+  end
+
+  def show
+    is_favourite = current_user.favourite_books.include?(@book)
+
+    response = {
+      book: @book,
+      isFavourite: is_favourite
+    }
+
+    json_response(response)
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
 end
--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,2 +1,27 @@
 class FavouritesController < ApplicationController
+  def index
+    @favourites = current_user.favourite_books
+
+    json_response(@favourites)
+  end
+
+  def create
+    current_user.favourites.create!(favourite_params)
+
+    response = { message: Message.favourite_created }
+    json_response(response, :created)
+  end
+
+  def destroy
+    current_user.favourites.where(favourite_params)[0].destroy
+
+    response = { message: Message.favourite_deleted }
+    json_response(response, :ok)
+  end
+
+  private
+
+  def favourite_params
+    params.permit(:book_id)
+  end
 end
