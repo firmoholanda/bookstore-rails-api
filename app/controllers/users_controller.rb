d78e6477dd416d5e1ef879e3bc5c0cb9aadbfbd2
firmo holanda <firmo@viize.com>
2020-09-02 12:37:39 -0300

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   def create
     user = User.create!(user_params)
     auth_token = AuthenticateUser.new(user.email, user.password).call
-    response = { message: Message.account_created, auth_token: auth_token }
+    response = { message: Message.user_created, auth_token: auth_token }
     json_response(response, :created)
   end
 
@@ -13,4 +13,5 @@ class UsersController < ApplicationController
   def user_params
     params.permit(:name, :email, :password, :password_confirmation)
   end
+  
 end
