078ef3250cc21155440a141b839a498813d963ac
firmo holanda <firmo@viize.com>
2020-09-02 14:47:00 -0300

    edit books and favourites controllers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,38 +1,33 @@
 class BooksController < ApplicationController
-  before_action :set_book, only: [:show]
+  before_action :set_book, only: [:show, :update, :destroy]
 
+  # GET /books
   def index
-    # get current user books
     @books = current_user.books
-    #@books = Book.all
     json_response(@books)
   end
 
   # POST /books
   def create
-    # create books belonging to current user
-    @book = current_user.books.create!(book_params)
-    json_response(@book, :created)
+    @book = current_user.books.create!(book_params)    
+    json_response({ message: Message.book_created })
   end
 
   # GET /books:id
   def show
-    is_favourite = current_user.favourite_books.include?(@book)
-    response = { book: @book, isFavourite: is_favourite }
-
-    json_response(response)
+    json_response(@book)
   end
 
   # PUT /books/:id
   def update
     @book.update(book_params)
-    head :no_content
+    json_response({ message: Message.book_updated })
   end
 
   # DELETE /books/:id
   def destroy
     @book.destroy
-    head :no_content
+    json_response({ message: Message.book_deleted })
   end
 
 
@@ -45,4 +40,5 @@ class BooksController < ApplicationController
   def set_book
     @book = Book.find(params[:id])
   end
+
 end
--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,23 +1,21 @@
 class FavouritesController < ApplicationController
   
+  # GET /favorites
   def index
     @favourites = current_user.favourite_books
-
     json_response(@favourites)
   end
 
+  # POST /favorites
   def create
-    current_user.favourites.create!(favourite_params)
-
-    response = { message: Message.favourite_created }
-    json_response(response, :created)
+    current_user.favourites.create!(favourite_params)    
+    json_response({ message: Message.favourite_created })
   end
-
+  
+  # DELETE /favorites
   def destroy
-    current_user.favourites.where(favourite_params)[0].destroy
-
-    response = { message: Message.favourite_deleted }
-    json_response(response, :ok)
+    current_user.favourites.where(favourite_params)[0].destroy    
+    json_response({ message: Message.favourite_deleted })
   end
 
   private
