39751f9e6a509a890daa6265396339423b2d08d5
firmo holanda <firmo@viize.com>
2020-09-03 17:04:40 -0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
-
+  
   # called before every action on controllers
   before_action :authorize_request
   attr_reader :current_user
-
+  
   private
 
   def authorize_request
