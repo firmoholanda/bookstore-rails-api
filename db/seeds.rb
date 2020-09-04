9f8725001b650812b88619877a42d0c5ffcfa3d2
firmo holanda <firmo@viize.com>
2020-09-04 12:42:15 -0300

    create db seed for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# create default user
+User.create!(name: "firmo holanda", email: "firmo@viize.com", password: "foobar", password_confirmation: "foobar")
+
+2.times do |n|
+  name = Faker::Name.name.downcase
+  email = "example-#{n+1}@example.com"
+  password="foobar"
+  password_confirmation="foobar"
+  User.create!(name: name, email: email, password: password, password_confirmation: password_confirmation)
+end
+
+# create books
+users = User.take(3)
+users.each do |user|
+  user_id = user.id
+  title = Faker::Book.title[0..10].downcase
+  description = Faker::Quote.yoda[0..30].downcase
+  category= Faker::Book.genre.downcase
+  Book.create!(user_id: user_id, title: title, description: description, category: category)
+end
+
+# create favourites
+user_id = User.first().id
+book_id = Book.first().id
+Favourite.create!(user_id: user_id, book_id: book_id)
