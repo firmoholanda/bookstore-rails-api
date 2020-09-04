94b1839580ef393e8b5cac4fc47382882e738bc0
firmo holanda <firmo@viize.com>
2020-09-04 12:47:19 -0300

    fix rubocop errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 # create default user
-User.create!(name: "firmo holanda", email: "firmo@viize.com", password: "foobar", password_confirmation: "foobar")
+User.create!(name: 'firmo holanda', email: 'firmo@viize.com', password: 'foobar', password_confirmation: 'foobar')
 
 2.times do |n|
   name = Faker::Name.name.downcase
-  email = "example-#{n+1}@example.com"
-  password="foobar"
-  password_confirmation="foobar"
+  email = "example-#{n + 1}@example.com"
+  password = 'foobar'
+  password_confirmation = 'foobar'
   User.create!(name: name, email: email, password: password, password_confirmation: password_confirmation)
 end
 
@@ -15,11 +15,11 @@ users.each do |user|
   user_id = user.id
   title = Faker::Book.title[0..10].downcase
   description = Faker::Quote.yoda[0..30].downcase
-  category= Faker::Book.genre.downcase
+  category = Faker::Book.genre.downcase
   Book.create!(user_id: user_id, title: title, description: description, category: category)
 end
 
 # create favourites
-user_id = User.first().id
-book_id = Book.first().id
+user_id = User.first.id
+book_id = Book.first.id
 Favourite.create!(user_id: user_id, book_id: book_id)
